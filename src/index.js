f180b8f07e3b4d62e6a5602ebf2e94b29b80753c
kickinbahk <kickinbahk@gmail.com>
2019-04-09 16:24:08 -0700

    add shopify specific vars

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import path from 'path'
 import express from 'express'
 
 const app = express(), DIST_DIR = __dirname, HTML_FILE = path.join(DIST_DIR, 'index.html')
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+const crypto = require('crypto');
+const cookie = require('cookie');
+const nonce = require('nonce')();
+const querystring = require('querystring');
+const request = require('request-promise');
+
+const apiKey = process.env.SHOPIFY_API_KEY;
+const apiSecret = process.env.SHOPIFY_API_SECRET;
+const scopes = 'read_products';
+const forwardingAddress = "{ngrok forwarding address}"; // Replace this with your HTTPS Forwarding address
 
 app.use(express.static(DIST_DIR))
 
@@ -15,3 +28,21 @@ app.listen(PORT, () => {
     console.log(`App listening to ${PORT}....`)
     console.log('Press Ctrl+C to quit.')
 })
+
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+    const redirectUri = forwardingAddress + '/shopify/callback';
+    const installUrl = 'https://' + shop +
+      '/admin/oauth/authorize?client_id=' + apiKey +
+      '&scope=' + scopes +
+      '&state=' + state +
+      '&redirect_uri=' + redirectUri;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+  } else {
+    return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
+});
