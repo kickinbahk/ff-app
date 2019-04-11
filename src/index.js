c3dba74eda520603cdb80b55fe8c14b57ade2dd4
kickinbahk <kickinbahk@gmail.com>
2019-04-11 09:53:56 -0700

    add api call to shop endpoint

--- src/index.js
+++ src/index.js
@@ -90,9 +90,18 @@ app.get('/shopify/callback', (req, res) => {
     .then((accessTokenResponse) => {
       const accessToken = accessTokenResponse.access_token;
     
-      res.status(200).send("Got an access token, let's do something with it");
-      // TODO
-      // Use access token to make API call to 'shop' endpoint
+      const shopRequestUrl = 'https://' + shop + '/admin/shop.json';
+      const shopRequestHeaders = {
+        'X-Shopify-Access-Token': accessToken,
+      };
+      
+      request.get(shopRequestUrl, { headers: shopRequestHeaders })
+      .then((shopResponse) => {
+        res.end(shopResponse);
+      })
+      .catch((error) => {
+        res.status(error.statusCode).send(error.error.error_description);
+      });
     })
     .catch((error) => {
       res.status(error.statusCode).send(error.error.error_description);
