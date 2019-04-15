115497cc77be44fb1375bf84ceca74878412c6d0
kickinbahk <kickinbahk@gmail.com>
2019-04-15 12:18:33 -0700

    fix db route call

--- index.js
+++ index.js
@@ -143,18 +143,17 @@ app.get('/shopify/callback', (req, res) => {
   } else {
     res.status(400).send('Required parameters missing');
   }
-
-  app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null };
-      res.render('pages/db', results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })  
-
 });
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null };
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});  
