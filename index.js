76e549c84691bb4cc4d24088985374570b5322da
kickinbahk <kickinbahk@gmail.com>
2019-04-23 09:47:47 -0700

    change html file back to html, change function to display the page on shopify

--- index.js
+++ index.js
@@ -124,8 +124,7 @@ app.get('/shopify/callback', (req, res) => {
 
       request.get(shopRequestUrl, { headers: shopRequestHeaders })
       .then((shopResponse) => {
-        res.status(200)
-        res.render('./views/pages/index')
+        res.sendFile('./views/pages/index')
       })
       .catch((error) => {
         res.status(error.statusCode).send(error.error.error_description);
