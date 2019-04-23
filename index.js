9b2e4b09bd6e11c9e0b964fd12da82f76893387b
kickinbahk <kickinbahk@gmail.com>
2019-04-23 10:51:01 -0700

    remove the json response

--- index.js
+++ index.js
@@ -130,9 +130,7 @@ app.get('/shopify/callback', (req, res) => {
 
       request.get(shopRequestUrl, { headers: shopRequestHeaders })
       .then((shopResponse) => {
-        res.status(200).end(
-          res.sendFile('index')
-        );
+        res.sendFile('index')
       })
       .catch((error) => {
         res.status(error.statusCode).send(error.error.error_description);
