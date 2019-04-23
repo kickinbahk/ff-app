7a3c476f4fa556dc538ad0f013af20e64295cd32
kickinbahk <kickinbahk@gmail.com>
2019-04-23 10:40:06 -0700

    add the sendfile function

--- index.js
+++ index.js
@@ -130,7 +130,9 @@ app.get('/shopify/callback', (req, res) => {
 
       request.get(shopRequestUrl, { headers: shopRequestHeaders })
       .then((shopResponse) => {
-        res.status(200).end(shopResponse);
+        res.status(200).end(
+          res.sendFile('index')
+        );
       })
       .catch((error) => {
         res.status(error.statusCode).send(error.error.error_description);
