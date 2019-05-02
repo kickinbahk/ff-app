4ca58407234c16ed8cf89ccf81802190b97e61af
kickinbahk <kickinbahk@gmail.com>
2019-05-02 14:12:19 -0700

    get the shopify route instead of callback

--- index.js
+++ index.js
@@ -133,44 +133,78 @@ app.post('/groups', async (req, res) => {
           res.json(group.toJSON())
         })
       }).then(function() {
+        app.get('/shopify', (req, res) => {
+          const { shop, hmac, code, state } = req.query;
+          console.log(req)
+          var stateCookie = '';
+        
+          if (req.headers.cookie) {
+            stateCookie = cookie.parse(req.headers.cookie).state; 
+          }
+        
+          if (state !== stateCookie) {
+            return res.status(403).send('Request origin cannot be verified');
+          }
+
+
+          if (shop && hmac && code) {
+            const map = Object.assign({}, req.query);
+            delete map['signature'];
+            delete map['hmac'];
+            const message = querystring.stringify(map);
+            const providedHmac = Buffer.from(hmac, 'utf-8');
+            const generatedHash = Buffer.from(
+              crypto
+                .createHmac('sha256', apiSecret)
+                .update(message)
+                .digest('hex'),
+                'utf-8'
+              );
+            let hashEquals = false;
+            // timingSafeEqual will prevent any timing attacks. Arguments must be buffers
+            try {
+              hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+            // timingSafeEqual will return an error if the input buffers are not the same length.
+            } catch (e) {
+              hashEquals = false;
+            };
             
-      const accessTokenRequestUrl = forwardingAddress + '/admin/oauth/access_token';
-      const accessTokenPayload = {
-        client_id: apiKey,
-        client_secret: apiSecret,
-        code,
-      };
-
-      request.post(accessTokenRequestUrl, { json: accessTokenPayload })
-      .then((accessTokenResponse) => {
-        const accessToken = accessTokenResponse.access_token;
-        // DONE: Use access token to make API call to 'shop' endpoint
-        const shopRequestUrl = 'https://' + shop + '/admin/api/2019-04/themes.json';
-        const shopRequestHeaders = {
-          'X-Shopify-Access-Token': accessToken,
-        };
-
-        request.get(shopRequestUrl, { headers: shopRequestHeaders })
-        .then((shopResponse) => {
-          res.status(200).end(shopResponse);
-        })
-        .catch((error) => {
-          res.status(error.statusCode).send(error.error.error_description);
+            if (!hashEquals) {
+              return res.status(400).send('HMAC validation failed');
+            }
+            
+            const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+            const accessTokenPayload = {
+              client_id: apiKey,
+              client_secret: apiSecret,
+              code,
+            };
+        
+            request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+            .then((accessTokenResponse) => {
+              const accessToken = accessTokenResponse.access_token;
+              // DONE: Use access token to make API call to 'shop' endpoint
+              const shopRequestUrl = 'https://' + shop + '/admin/api/2019-04/themes.json';
+              const shopRequestHeaders = {
+                'X-Shopify-Access-Token': accessToken,
+              };
+        
+              request.get(shopRequestUrl, { headers: shopRequestHeaders })
+              .then((shopResponse) => {
+                console.log(shopResponse)
+              })
+              .catch((error) => {
+                res.status(error.statusCode).send(error.error.error_description);
+              });
+            })
+            .catch((error) => {
+              res.status(error.statusCode).send(error.error.error_description);
+            });
         });
-      })
-      .catch((error) => {
-        res.status(error.statusCode).send(error.error.error_description);
-      });
 
-      request.post(accessTokenRequestUrl, { json: accessTokenPayload })
-      .then((accessTokenResponse) => {
-        res.render('views/pages/index.html');
+      }, function (e) {
+        res.status(400).json(e)
       })
-      .catch((error) => {
-        res.status(error.statusCode).send(error.error.error_description);
-      });
-
-    });
     } else {
       res.status(506).send({ error: 'Group ID already exists' });
     }
